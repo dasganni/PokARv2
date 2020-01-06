4ac34257a5db6ada749305eac3bf535ac4e3de55
Mirko Leschhorn <20495026+dasganni@users.noreply.github.com>
2020-01-06 11:21:53 +0100

    Changed to show winning cards instead of all cards of a player

--- Poker_Game.py
+++ Poker_Game.py
@@ -39,6 +39,12 @@ class Hand:
             handcards = handcards + c.display() + ', '
         return handcards
 
+    def print_bestHand(self):
+        handcards=''
+        for c in self.counting_cards:
+            handcards = handcards + c.display() + ', '
+        return handcards
+
     def is_straight(self):
         poker_card_values=[]
         straight_count = 0
